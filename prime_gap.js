cd4002f8110e2364d95e6d18d265d6ee864fd7c7
James Stack <jstack@Jamess-MacBook-Pro.local>
2017-04-17 13:01:57 -0700

    Add prime gap

--- prime_gap.js
+++ prime_gap.js
@@ -1,1 +1,45 @@
-This is the first file
+function gap(g, m, n){
+  let result = [];
+
+  for (var i = m; i + g < n; i++) {
+    if (prime(i) && prime(i + g)) {
+      for (var j = i + 1; j < g; j++) {
+        if (prime(j)) {
+          next;
+        }
+      }
+      return [i, i + g];
+    }
+  }
+  return null;
+}
+
+function prime(num){
+  if (num < 2) {
+    return false;
+  }
+
+  if (num === 2) {
+    return true;
+  }
+
+
+  let maxEnd = Math.floor(Math.sqrt(num));
+  for (var i = 2; i <= maxEnd; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let test = gap(2, 100, 110);
+let test1 = gap(4, 100, 110);
+let test2 = gap(6, 100, 110);
+let test3 = gap(8, 300, 400);
+let test4 = gap(10, 300, 400);
+console.log(test);
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
