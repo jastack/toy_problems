916924640f693ff08304ff902423038719df5a19
James Stack <james.a.stack@gmail.com>
2017-04-22 20:44:38 -0700

    Add solution

--- parens.js
+++ parens.js
@@ -1,3 +1,9 @@
 function validParentheses(parens){
-  // TODO
+  var indent = 0;
+
+  for (var i = 0 ; i < parens.length && indent >= 0; i++) {
+    indent += (parens[i] == '(') ? 1 : -1;
+  }
+
+  return (indent == 0);
 }
