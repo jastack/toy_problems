0db65566abf24378181c0e445df689db182c00e5
James Stack <james.a.stack@gmail.com>
2017-05-01 11:13:02 -0700

    finalized fibs problem

--- fibs.js
+++ fibs.js
@@ -1,14 +1,22 @@
 function perimeter(n) {
-  let sum = 0
-  let arr = [1, 1]
+  let sum = 4;
+  let arr = [1, 1, 2];
 
   if (n === 1){
-    return 4;
-  } else if (n === 2) {
     return 8;
+  } else if (n === 2) {
+    return 16;
   }
+
+  for (var i = 3; i < n + 1; i++) {
+    let update = arr[i - 1] + arr[i - 2];
+    arr.push(update);
+    sum += update;
+  }
+
+  return 4 * sum;
 
 }
 
-let result = perimeter(5);
+let result = perimeter(30);
 console.log(result);
