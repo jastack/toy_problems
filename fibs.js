e92a3ef37401533495e8036969ab9bb0e076ead9
James Stack <james.a.stack@gmail.com>
2017-05-01 11:04:50 -0700

    add perimeter problem

---
+++ fibs.js
@@ -0,0 +1,14 @@
+function perimeter(n) {
+  let sum = 0
+  let arr = [1, 1]
+
+  if (n === 1){
+    return 4;
+  } else if (n === 2) {
+    return 8;
+  }
+
+}
+
+let result = perimeter(5);
+console.log(result);
