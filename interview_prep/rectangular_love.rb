0356efb1443416bb5368db4ca669607de1ccc3a3
James Stack <james.a.stack@gmail.com>
2017-08-07 15:10:27 -0700

    add love rectangle

---
+++ interview_prep/rectangular_love.rb
@@ -0,0 +1,11 @@
+# Given two rectangle objects, return where they overlap,
+# as a rectangle objects
+
+my_rectangle = {
+  'x' => 1,
+  'y' => 5,
+  'width' => 10,
+  'height' => 4
+}
+
+p my_rectangle['width']
