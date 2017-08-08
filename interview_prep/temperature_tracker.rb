7fb91d4bc3c2e5d41d3fad3b4ee82bdd1605449b
James Stack <james.a.stack@gmail.com>
2017-08-08 16:20:18 -0700

    Finished temp problem

--- interview_prep/temperature_tracker.rb
+++ interview_prep/temperature_tracker.rb
@@ -3,12 +3,33 @@ class TempTracker
   def initialize()
     @max = nil
     @min = nil
-    @mean = nil
+    @sum = 0
     @mode = nil
+    @num = 0
   end
 
   def insert(temp)
+    @sum += temp
+    @num += 1
+    if @max == nil || temp > @max
+      @max = temp
+    end
 
+    if @min == nil || temp < @min
+      @min = temp
+    end
+  end
+
+  def get_max
+    @max
+  end
+
+  def get_min
+    @min
+  end
+
+  def get_mean
+    @sum / @num
   end
 
 end
