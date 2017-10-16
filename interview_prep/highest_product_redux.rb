f246b3b08ad7bdc1f7e0033522442a357c8501dd
James Stack <james.a.stack@gmail.com>
2017-10-16 10:58:25 -0700

    Add mock interview prep

---
+++ interview_prep/highest_product_redux.rb
@@ -0,0 +1,11 @@
+def highest_product(arr)
+  if arr.length < 3
+    return "Array not long enough!"
+  end
+
+
+end
+
+arr = [1,2,3,4,5]
+
+p highest_product(arr)
--- interview_prep/tree_sum.js
+++ interview_prep/tree_sum.js
@@ -4,24 +4,39 @@ function Node(val){
   this.right = null;
 }
 
-let nodeOne = new Node(4);
-let nodeTwo = new Node(1);
-let nodeThree = new Node(3);
-let nodeFour = new Node(-2);
-let nodeFive = new Node(1);
-let nodeSix = new Node(2);
+// let nodeOne = new Node(4);
+// let nodeTwo = new Node(1);
+// let nodeThree = new Node(3);
+// let nodeFour = new Node(-2);
+// let nodeFive = new Node(1);
+// let nodeSix = new Node(2);
+// let nodeSeven = new Node(3);
+// let nodeEight = new Node(-2);
+// let nodeNine = new Node(-3);
+//
+// nodeOne.left = nodeTwo;
+// nodeOne.right = nodeThree;
+// nodeTwo.left = nodeFour;
+// nodeThree.left = nodeFive;
+// nodeThree.right = nodeSix;
+// nodeFour.right = nodeSeven;
+// nodeSix.left = nodeEight;
+// nodeSix.right = nodeNine;
+
+let nodeOne = new Node(1);
+let nodeTwo = new Node(2);
+let nodeThree = new Node(2);
+let nodeFour = new Node(3);
+let nodeFive = new Node(4);
+let nodeSix = new Node(4);
 let nodeSeven = new Node(3);
-let nodeEight = new Node(-2);
-let nodeNine = new Node(-3);
 
 nodeOne.left = nodeTwo;
 nodeOne.right = nodeThree;
-nodeTwo.left = nodeFour;
-nodeThree.left = nodeFive;
-nodeThree.right = nodeSix;
-nodeFour.right = nodeSeven;
-nodeSix.left = nodeEight;
-nodeSix.right = nodeNine;
+// nodeTwo.left = nodeFour;
+nodeTwo.right = nodeFive;
+nodeThree.left = nodeSix;
+nodeThree.right = nodeSeven;
 
 // console.log(nodeOne);
 
@@ -45,4 +60,55 @@ function pathSum(tree, sum){
   return search(tree, 0);
 }
 
-console.log(pathSum(nodeOne, 7));
+function checkSymmetric(arr){
+  let start = 0;
+  let end = arr.length - 1;
+  let i = 0;
+  while (start < end){
+    if (arr[start] !== arr[end]){
+      return false;
+    } else {
+      i += 1;
+      start += 1;
+      end -= 1;
+    }
+  }
+
+  return true;
+}
+
+console.log(checkSymmetric([1,null,2,1]));
+
+function isTreeSymmetric(tree){
+  let queue = [tree];
+  // console.log(queue);
+  let i = 0;
+  let level = 0;
+  let levelPointer = 0;
+  while (queue[i]){
+    let currentNode = queue[i];
+    queue.push(currentNode.left);
+    queue.push(currentNode.right);
+    if (i === Math.pow(2, level)){
+      // console.log(queue.slice(levelPointer));
+      level += 1;
+      levelPointer += Math.pow(2, level);
+
+    }
+
+    i++;
+  }
+
+  function readTree(arr){
+    return arr.map(el => {
+      if (el){
+        return el.val;
+      }
+    });
+  }
+
+  return readTree(queue);
+}
+
+console.log(isTreeSymmetric(nodeOne));
+// console.log(pathSum(nodeOne, 7));
